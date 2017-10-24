6abd9c6fc09b5da0cfc31315f55cf10797fd0d8a
zhaoyongxin <zhaoyongxin@hotmail.com>
2017-10-24 21:33:10 +0800

    python修改

--- hellopython.py
+++ hellopython.py
@@ -1,1 +1,18 @@
-print('my python test')
+import random
+secret = random.randint(1, 10)
+print(secret)
+print('------------------学习python------------------')
+temp = input('猜数字:')
+guess = int(temp)
+while guess != secret:
+    temp = input('请重新猜数字:')
+    guess = int(temp)
+    if guess == secret:
+        print('猜中了')
+    else:
+        if guess > secret:
+            print("猜大了")
+        else:
+            print("猜小了")
+        print('猜错哦')
+print('游戏结束')
